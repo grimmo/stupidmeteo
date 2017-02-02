caf8743023118d923f5e565ba8ab7837e17848b5
grimmo <luigi.messina@grimmo.it>
2017-02-02 10:16:24 +0100

    - Added sample configuration file

---
+++ config.sample.py
@@ -0,0 +1,3 @@
+API_KEY='<Your Dark Sky API key goes here>'
+DOWNLOAD_FOLDER = '<Absolute path to a filesystem folder, preferably tmpfs>'
+UPLOAD_FOLDER = '<Absolute path to a filesystem folder, preferably tmpfs>'
